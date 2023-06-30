13c2adefc43c3665726cd8126bcd4cdb6e56de1f
Dustin <135719141+Dustin-thirdweb@users.noreply.github.com>
2023-06-29 23:02:26 -0500

    Update index.tsx

--- pages/nfts/index.tsx
+++ pages/nfts/index.tsx
@@ -1,13 +1,11 @@
 import type { NextPage } from "next";
 import styles from "../../styles/Home.module.css";
 import UseAirdropHook from "../../live-code/snippets/nfts/UseAirdropHook";
-import UseBrunNFT from "../../live-code/snippets/nfts/UseBrunNFT";
+import UseBurnNFT from "../../live-code/snippets/nfts/UseBurnNFT";
 import UseMintNFTSupply from "../../live-code/snippets/nfts/UseMintNFTSupply";
 import UseNFTBalance from "../../live-code/snippets/nfts/UseNFTBalance";
 import UseNFTHook from "../../live-code/snippets/nfts/UseNFTHook";
 import UseOwnedNFTs from "../../live-code/snippets/nfts/UseOwnedNFTs";
-import UseTotalCirculatingSupply from "../../live-code/snippets/nfts/UseTotalCirculatingSupply";
-import UseTotalCount from "../../live-code/snippets/nfts/UseTotalCount";
 import UseTransferNFT from "../../live-code/snippets/nfts/UseTransferNFT";
 import UseLazyMint from "../../live-code/snippets/nfts/nftdrops/UseLazyMint";
 import UseClaimNFT from "../../live-code/snippets/nfts/nftdrops/UseClaimNFT";
@@ -26,18 +24,18 @@ const NFTs: NextPage = () => {
         <UseClaimNFT />
         <h2>useUnclaimedNFTs</h2>
         <UseUnclaimedNFTs />
-        <h2>useTotalCirculatingSupply</h2>
+        {/* <h2>useTotalCirculatingSupply</h2>
         <UseTotalCirculatingSupply />
         <h2>useTotalCount</h2>
-        <UseTotalCount />
+        <UseTotalCount /> */}
         <h2>useTransferNFT</h2>
         <UseTransferNFT />
         <h2>useAirdrop</h2>
         <UseAirdropHook />
         <h2>useBurnNFT</h2>
-        <UseBrunNFT />
-        <h2>useMintNFTSupply</h2>
-        <UseMintNFTSupply />
+        <UseBurnNFT />
+        {/* <h2>useMintNFTSupply</h2>
+        <UseMintNFTSupply /> */}
         <h2>useNFTBalance</h2>
         <UseNFTBalance />
       </main>
