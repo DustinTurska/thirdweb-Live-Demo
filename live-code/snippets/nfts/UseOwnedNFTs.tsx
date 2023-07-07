dac85fb6d9ef462321c557c20ac7ee04b62ffbf7
Dustin <135719141+Dustin-thirdweb@users.noreply.github.com>
2023-07-07 18:14:24 -0500

    Update UseOwnedNFTs.tsx

--- live-code/snippets/nfts/UseOwnedNFTs.tsx
+++ live-code/snippets/nfts/UseOwnedNFTs.tsx
@@ -6,12 +6,11 @@ const scope = { React, ThirdwebProvider, useOwnedNFTs, useContract, useAddress  
 
 const code = `function App() {
   const address = useAddress();
-  const { contract } = useContract("0x5907C77D0b413304667d1764b479ED70eF659e52");
+  const { contract } = useContract("0x94221e64B4f64741534D4F65DCf91842A1cA6ae5");
   const { data, isLoading, error } = useOwnedNFTs(contract, address);
 
-  // Return statement with the desired data structure and max width applied
   return (
-    <div>
+    <div style={{ height: "600px", overflow: "auto" }}>
       {data && data.map((nft) => (
         <div key={nft.metadata.id}>
           <h2>{nft.metadata.name}</h2>
@@ -19,20 +18,21 @@ const code = `function App() {
           <img
             src={nft.metadata.image}
             alt={nft.metadata.name}
-            style={{ maxWidth: "300px" }} // Apply max width of 300px
+            style={{ maxWidth: "300px" }}
           />
-          {/* Render other properties as needed */}
           <p>Owner: {nft.owner}</p>
           <p>Type: {nft.type}</p>
           <p>Supply: {nft.supply}</p>
-          {/* Render quantityOwned only for ERC1155 */}
           {nft.type === "ERC1155" && <p>Quantity Owned: {nft.quantityOwned}</p>}
         </div>
       ))}
     </div>
   );
-}`;
+}
+`;
 
 export default function UseOwnedNFTs() {
-  return <LiveCodeEditor code={code} additionalScope={scope} />;
+  return <LiveCodeEditor code={code} additionalScope={scope} 
+    network=""
+  />;
 }
